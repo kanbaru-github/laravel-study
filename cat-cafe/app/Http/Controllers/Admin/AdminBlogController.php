de4c7e4d6ef3732b315af590d1ff94156391335c
kanbaru-github <usupfv@gmail.com>
2023-04-13 16:23:08 +0900

    completed section 15

--- cat-cafe/app/Http/Controllers/Admin/AdminBlogController.php
+++ cat-cafe/app/Http/Controllers/Admin/AdminBlogController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\StoreBlogRequest;
 use App\Http\Requests\Admin\UpdateBlogRequest;
 use App\Models\Blog;
+use App\Models\Category;
+use App\Models\Cat;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -54,7 +56,9 @@ class AdminBlogController extends Controller
      */
     public function edit(Blog $blog)
     {
-		return view('admin.blogs.edit', ['blog' => $blog]);
+		$categories = Category::all();
+		$cats = Cat::all();
+		return view('admin.blogs.edit', ['blog' => $blog, 'categories' => $categories, 'cats' => $cats]);
     }
 
     /**
@@ -70,6 +74,8 @@ class AdminBlogController extends Controller
 			Storage::disk('public')->delete($blog->image);
 			$updateData['image'] = $request->file('image')->store('blogs', 'public');
 		}
+		$blog->category()->associate($updateData['category_id']);
+		$blog->cats()->sync($updateData['cats'] ?? []);
 		$blog->update($updateData);
 
 		return to_route('admin.blogs.index')->with('success', 'ブログを更新しました');
