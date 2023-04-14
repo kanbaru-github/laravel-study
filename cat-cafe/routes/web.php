164a63a668feaa45e7f8597f3c6466212d7e3528
kanbaru-github <usupfv@gmail.com>
2023-04-14 22:52:59 +0900

    completed section 16

--- cat-cafe/routes/web.php
+++ cat-cafe/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminBlogController;
+use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
@@ -24,10 +26,27 @@ Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 Route::post('/contact', [ContactController::class, 'sendMail']);
 Route::get('/contact/complete', [ContactController::class, 'complete'])->name('contact.complete');
 
-// ブログ
-Route::get('/admin/blogs', [AdminBlogController::class, 'index'])->name('admin.blogs.index');
-Route::get('/admin/blogs/create', [AdminBlogController::class, 'create'])->name('admin.blogs.create');
-Route::post('/admin/blogs', [AdminBlogController::class, 'store'])->name('admin.blogs.store');
-Route::get('/admin/blogs/{blog}', [AdminBlogController::class, 'edit'])->name('admin.blogs.edit');
-Route::put('/admin/blogs/{blog}', [AdminBlogController::class, 'update'])->name('admin.blogs.update');
-Route::delete('/admin/blogs/{blog}', [AdminBlogController::class, 'destroy'])->name('admin.blogs.destroy');
+// 管理画面
+Route::prefix('/admin')
+	->name('admin.')
+	->group(function() {
+		// ログイン時のみアクセス可能
+		Route::middleware('auth')
+			->group(function() {
+				// ブログ
+				Route::resource('/blogs', AdminBlogController::class)->except('show');
+		
+				Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+			});
+		// 未ログイン時のみアクセス可能
+		Route::middleware('guest')
+			->group(function() {
+				// 認証
+				Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+				Route::post('/login', [AuthController::class, 'login']);
+			});
+	});
+
+// ユーザー管理
+Route::get('/admin/users/create', [UserController::class, 'create'])->name('admin.users.create');
+Route::post('/admin/users', [UserController::class, 'store'])->name('admin.users.store');
